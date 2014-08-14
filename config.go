b4d059d8e63749ce311766c93a65f0fba88d51a2
Katherine Cox <cox.katherine.e@gmail.com>
2014-08-14 09:56:34 -0500

    Backport: Updating goyaml to gopkg.in path for bugfix.
    Conflicts:
    	bundledata.go
    	charmarchive_test.go
    	meta.go

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 
 	"github.com/juju/schema"
-	"launchpad.net/goyaml"
+	goyaml "gopkg.in/yaml.v1"
 )
 
 // Settings is a group of charm config option names and values. A Settings
