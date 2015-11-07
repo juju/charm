6213a60a56a7b55e7208cf9ea97dd77f80b4ddc6
Nate Finch <nate.finch@gmail.com>
2015-11-07 01:11:20 -0500

    use github.com/juju/version

--- meta.go
+++ meta.go
@@ -12,9 +12,9 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/juju/juju/version"
 	"github.com/juju/schema"
 	"github.com/juju/utils"
+	"github.com/juju/version"
 	"gopkg.in/yaml.v1"
 
 	"gopkg.in/juju/charm.v6-unstable/hooks"
